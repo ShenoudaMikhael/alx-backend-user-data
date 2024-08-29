776d289b4bed13945d6b1f47f3a2c6a0a989d320
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-29 23:57:14 +0300

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 """filtered_logger Module"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """filter_datum function"""
     for field in fields:
-        message = re.sub(f"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}", message)
+        message = re.sub(
+            f"{field}=.*?{separator}",
+            f"{field}={redaction}{separator}", message
+        )
     return message
