e97ab95f7186b99ce030fa781fabb97b72e49bc8
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-02 23:31:20 +0300

    3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Auth module"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth function"""
+        # return path if path not in excluded_paths else False
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header function"""
+        return request
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """current_user function"""
+        return request if request else None
