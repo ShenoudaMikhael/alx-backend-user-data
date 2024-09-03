c2112d08f51a0ee843be57d240567d49a59b7e04
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-03 10:23:33 +0300

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,3 +17,18 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """decode base64 authorization header"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            a = base64_authorization_header.decode("utf-8")
+            return a
+        except Exception:
+            return None
