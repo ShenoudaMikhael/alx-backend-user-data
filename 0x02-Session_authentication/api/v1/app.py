8bfbae6cee03eb9a02ff777f75b8b81d4ef2fd6c
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-05 11:06:19 +0300

    0. Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -16,12 +16,15 @@ auth = None
 AUTH_TYPE = getenv("AUTH_TYPE", None)
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
+
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
+
     auth = BasicAuth()
 elif AUTH_TYPE == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
+
     auth = SessionAuth()
 
 
@@ -52,13 +55,16 @@ def before_request_handler() -> str:
         "/api/v1/status/",
         "/api/v1/unauthorized/",
         "/api/v1/forbidden/",
-        "/api/v1/auth_session/login/"
-        ]
+        "/api/v1/auth_session/login/",
+    ]
     if auth.require_auth(request.path, ex_path) is False:
         return
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if (
+        auth.authorization_header(request) is None
+        and auth.session_cookie(request) is None
+    ):
         abort(401)
     current = auth.current_user(request)
     if current is None:
