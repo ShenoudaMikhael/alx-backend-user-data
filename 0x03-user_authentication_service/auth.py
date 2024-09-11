397e7e3ec44ed005638e52e131835ce9fdadee87
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-11 23:31:19 +0300

    4. Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 """Auth Module"""
 import bcrypt
-from db import DB
-from user import User
-from sqlalchemy.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """hash password function"""
-
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
